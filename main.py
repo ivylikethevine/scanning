e7c907a8b200c9f2da5a643b8789530c18d96406
Ivy Duggan <ivylikethevine@gmail.com>
2023-10-29 13:58:35 -0700

    simpler attempt

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from gpiozero import Button
-# import time
+import time
 import cv2
 
 cam = cv2.VideoCapture(0)
 
-bounce_time = 1.0
-num_pics = 50
-count = 0
+bounce_time = 0.2000
+# num_pics = 50
+# count = 0
 
 def take_picture_with_camera():
     print('Button pressed')
@@ -16,17 +16,18 @@ def take_picture_with_camera():
     else:
         # image_path = '/home/ivy/scanner/scans/image_%s.jpg' % int(
         #     round(time.time() * 1000))
-        image_path = '/home/ivy/scanner/scans/%s.jpg' % count
+        image_path = '/home/ivy/scanner/scans/' + time.time() + '.jpg'
         cv2.imwrite(image_path, image)
-        count += 1
-        print('Pic #' + count)
+        # count += 1
+        # print('Pic #' + count)
 
 button = Button(2, bounce_time=bounce_time)
 button.when_pressed = take_picture_with_camera
 
 def run():
     print('Ready!')
-    while count < num_pics:
+    # while count < num_pics:
+    while True:
         pass
     cam.release()
 
