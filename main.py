4381d61dd6bfe7e54d54d2ed2424cd2cf78932cc
Ivy Duggan <ivylikethevine@gmail.com>
2023-10-29 15:42:43 -0700

    new cam settings

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ import time
 import cv2
 
 cam = cv2.VideoCapture(0)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1080)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1920)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1920)
+cam.set(cv2.CAP_PROP_FPS, 10)
 
-pics = 0
 bounce_time = 0.100
 
-def take_picture_with_camera(pics):
+def take_picture_with_camera():
     ret, image = cam.read()
     if ret:
         image_path = '/home/ivy/scanner/scans/image_%s.jpg' % int(
             round(time.time() * 1000))
         cv2.imwrite(image_path, image)
-        pics += 1
-        print(pics)
+        print('Pic taken')
 
 button = Button(2, bounce_time=bounce_time)
-button.when_pressed = lambda pics : take_picture_with_camera(pics)
+button.when_released = take_picture_with_camera
 
 def run():
     print('Ready!')
