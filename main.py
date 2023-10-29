442462fa0d65a325e62325bc3bf13443c120c104
Ivy Duggan <ivylikethevine@gmail.com>
2023-10-29 14:04:47 -0700

    still having trouble with file saving

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ import cv2
 
 cam = cv2.VideoCapture(0)
 
-bounce_time = 0.2000
-# num_pics = 50
-# count = 0
+bounce_time = 0.150
 
 def take_picture_with_camera():
     print('Button pressed')
@@ -14,19 +12,15 @@ def take_picture_with_camera():
     if not ret:
         print('error taking picture')
     else:
-        # image_path = '/home/ivy/scanner/scans/image_%s.jpg' % int(
-        #     round(time.time() * 1000))
-        image_path = '/home/ivy/scanner/scans/' + time.time() + '.jpg'
+        image_path = '/home/ivy/scanner/scans/image_%s.jpg' % int(
+            round(time.time() * 1000))
         cv2.imwrite(image_path, image)
-        # count += 1
-        # print('Pic #' + count)
 
 button = Button(2, bounce_time=bounce_time)
 button.when_pressed = take_picture_with_camera
 
 def run():
     print('Ready!')
-    # while count < num_pics:
     while True:
         pass
     cam.release()
