968bb5553a4c77ebae0add0e2f847093112be076
Ivy Duggan <ivylikethevine@gmail.com>
2023-10-29 15:53:25 -0700

    even more logging

--- main.py
+++ main.py
@@ -8,15 +8,14 @@ cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1920)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1920)
 cam.set(cv2.CAP_PROP_FPS, 10)
 
-bounce_time = 0.100
+bounce_time = 0.200
 
 def take_picture_with_camera():
     ret, image = cam.read()
     if ret:
-        image_path = '/home/ivy/scanner/scans/image_%s.jpg' % int(
+        image_path = './scans/image_%s.jpg' % int(
             round(time.time() * 1000))
-        cv2.imwrite(image_path, image)
-        print('Pic taken')
+        print(cv2.imwrite(image_path, image) + ' | ' + image_path)
 
 button = Button(2, bounce_time=bounce_time)
 button.when_released = take_picture_with_camera
