5e9682baf79f9a889ccc8ff3de4521128a5a0984
Ivy Duggan <ivylikethevine@gmail.com>
2023-10-29 15:30:18 -0700

    counter hopefully

--- main.py
+++ main.py
@@ -4,21 +4,17 @@ import cv2
 
 cam = cv2.VideoCapture(0)
 
-bounce_time = 0.150
+pics = 0
+bounce_time = 0.100
 
 def take_picture_with_camera():
-    print('Button pressed')
     ret, image = cam.read()
-    print('cam.read')    
-    if not ret:
-        print('error taking picture')
-    else:
-        print('creating image path')
+    if ret:
         image_path = '/home/ivy/scanner/scans/image_%s.jpg' % int(
             round(time.time() * 1000))
-        print('writing image')
         cv2.imwrite(image_path, image)
-    print('Image function over')
+        pics += 1
+        print(pics)
 
 button = Button(2, bounce_time=bounce_time)
 button.when_pressed = take_picture_with_camera
