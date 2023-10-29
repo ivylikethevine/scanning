393662480528a19f2d21d6b3e8024a209a207f36
Ivy Duggan <ivylikethevine@gmail.com>
2023-10-29 16:02:33 -0700

    revised loop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from gpiozero import Button
 import time
 import cv2
+import fnmatch
+import os
 
 cam = cv2.VideoCapture(0)
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1080)
@@ -8,21 +10,26 @@ cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1920)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1920)
 cam.set(cv2.CAP_PROP_FPS, 10)
 
-bounce_time = 0.200
+num_pics = 50
+bounce_time = 0.150
 
 def take_picture_with_camera():
     ret, image = cam.read()
     if ret:
         image_path = './scans/image_%s.jpg' % int(
             round(time.time() * 1000))
-        print(cv2.imwrite(image_path, image) + ' | ' + image_path)
+        print(cv2.imwrite(image_path, image))
 
 button = Button(2, bounce_time=bounce_time)
 button.when_released = take_picture_with_camera
 
 def run():
+    count = 0
     print('Ready!')
-    while True:
+    while count < num_pics:
+        dir_path = './scans/'
+        count = len(fnmatch.filter(os.listdir(dir_path), '*.*'))
+        print('Count:', count)
         pass
     cam.release()
 
