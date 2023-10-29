65676498c47cf9197e03b10300c29b4c1b50ee36
Ivy Duggan <ivylikethevine@gmail.com>
2023-10-29 15:11:10 -0700

    more logging!

--- main.py
+++ main.py
@@ -9,12 +9,16 @@ bounce_time = 0.150
 def take_picture_with_camera():
     print('Button pressed')
     ret, image = cam.read()
+    print('cam.read')    
     if not ret:
         print('error taking picture')
     else:
+        print('creating image path')
         image_path = '/home/ivy/scanner/scans/image_%s.jpg' % int(
             round(time.time() * 1000))
+        print('writing image')
         cv2.imwrite(image_path, image)
+    print('Image function over')
 
 button = Button(2, bounce_time=bounce_time)
 button.when_pressed = take_picture_with_camera
